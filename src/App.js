c33daf9e4569d291eb78cb0c4365ff5ace3696a0
menomenonkwan <menomenonkwan@gmail.com>
2021-11-17 18:19:05 -0800

    completed

--- src/App.js
+++ src/App.js
@@ -24,6 +24,8 @@ function App() {
       .sort(() => Math.random() - 0.5)
       .map(card => ({ ...card, id: Math.random() }));
 
+    setChoiceOne(null);
+    setChoiceTwo(null);
     setCards(shuffledCards);  
     setTurns(0);
   }
@@ -64,6 +66,11 @@ function App() {
     setDisabled(false);
   }
 
+  // start a new game
+  useEffect(() => {
+    shuffleCards();
+  }, []);
+
   return (
     <div className="App">
       <h1>Magic Match</h1>
@@ -80,7 +87,7 @@ function App() {
           />
         ))}
       </div>
-
+      <p>Turns: {turns}</p>
     </div>
   );
 }
