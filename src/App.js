ca28cf371f13516048068357d3a6d67b90aa9d58
menomenonkwan <menomenonkwan@gmail.com>
2021-11-17 18:12:37 -0800

    finish game functionality

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import './App.css'
 import SingleCard from './components/SingleCard';
 
 const cardImages = [
-  { "src": "/img/helmet-1.png" },
-  { "src": "/img/potion-1.png" },
-  { "src": "/img/ring-1.png" },
-  { "src": "/img/scroll-1.png" },
-  { "src": "/img/shield-1.png" },
-  { "src": "/img/sword-1.png" },
+  { "src": "/img/helmet-1.png", matched: false },
+  { "src": "/img/potion-1.png", matched: false },
+  { "src": "/img/ring-1.png", matched: false },
+  { "src": "/img/scroll-1.png", matched: false },
+  { "src": "/img/shield-1.png", matched: false },
+  { "src": "/img/sword-1.png", matched: false },
 ]
 
 function App() {
@@ -16,6 +16,7 @@ function App() {
   const [turns, setTurns] = useState(0);
   const [choiceOne, setChoiceOne] = useState(null);
   const [choiceTwo, setChoiceTwo] = useState(null);
+  const [disabled, setDisabled] = useState(false);
 
   // shuffle cards
   const shuffleCards = () => {
@@ -34,9 +35,24 @@ function App() {
 
   // compare choices
   useEffect(() => {
-    if(choiceTwo) {
-      choiceOne.src === choiceTwo.src ? console.log('it matches') : console.log('nope');
-      resetTurn();
+    if(choiceOne && choiceTwo) {
+      setDisabled(true);
+
+      if (choiceOne.src === choiceTwo.src) {
+        setCards(prevCards => {
+          return prevCards.map(card => {
+            if (card.src ===  choiceOne.src) {
+              return { ...card, matched: true }
+            } else {
+              return card;
+            }
+          })
+        })
+        resetTurn();
+      } else {
+        setTimeout(() => resetTurn(), 1000);
+      }
+
     }
   }, [choiceOne, choiceTwo]);
 
@@ -45,6 +61,7 @@ function App() {
     setChoiceOne(null);
     setChoiceTwo(null);
     setTurns(prevTurns => prevTurns + 1);
+    setDisabled(false);
   }
 
   return (
@@ -58,6 +75,8 @@ function App() {
             key={card.id} 
             card={card} 
             handleChoice={handleChoice}
+            flipped={card === choiceOne || card === choiceTwo || card.matched }
+            disabled={disabled}
           />
         ))}
       </div>
