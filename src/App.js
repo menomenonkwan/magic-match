8adf704b4a1b23add9a491f854cb9cd79a9fd32c
menomenonkwan <menomenonkwan@gmail.com>
2021-11-17 17:46:49 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import { useEffect, useState } from 'react';
+import './App.css'
+import SingleCard from './components/SingleCard';
+
+const cardImages = [
+  { "src": "/img/helmet-1.png" },
+  { "src": "/img/potion-1.png" },
+  { "src": "/img/ring-1.png" },
+  { "src": "/img/scroll-1.png" },
+  { "src": "/img/shield-1.png" },
+  { "src": "/img/sword-1.png" },
+]
+
+function App() {
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0);
+  const [choiceOne, setChoiceOne] = useState(null);
+  const [choiceTwo, setChoiceTwo] = useState(null);
+
+  // shuffle cards
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map(card => ({ ...card, id: Math.random() }));
+
+    setCards(shuffledCards);  
+    setTurns(0);
+  }
+
+  // handle choice
+  const handleChoice = (card) => {
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
+  }
+
+  // compare choices
+  useEffect(() => {
+    if(choiceTwo) {
+      choiceOne.src === choiceTwo.src ? console.log('it matches') : console.log('nope');
+      resetTurn();
+    }
+  }, [choiceOne, choiceTwo]);
+
+  // reset choices and increase turns
+  const resetTurn = () => {
+    setChoiceOne(null);
+    setChoiceTwo(null);
+    setTurns(prevTurns => prevTurns + 1);
+  }
+
+  return (
+    <div className="App">
+      <h1>Magic Match</h1>
+      <button onClick={shuffleCards}>New Game</button>
+
+      <div className="card-grid">
+        {cards.map(card => (
+          <SingleCard 
+            key={card.id} 
+            card={card} 
+            handleChoice={handleChoice}
+          />
+        ))}
+      </div>
+
+    </div>
+  );
+}
+
+export default App
